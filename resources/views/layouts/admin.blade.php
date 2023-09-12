e172bbe111fb15403a5991e23c007662de8279db
Xuan Trung <xuantrung276@gmail.com>
2023-09-12 11:25:43 +0700

    Admin Panel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{ asset('admin/css/nucleo-svg.css" rel="stylesheet') }}" />
+    <link href="{{ asset('admin/css/nucleo-icons.css" rel="stylesheet') }}" />
+    <link href="{{ asset('admin/css/nucleo-svg.css" rel="stylesheet') }}" />
+
+    <!-- Styles -->
+    <link href="{{ asset('admin/css/soft-ui-dashboard.css?v=1.0.7') }}" rel="stylesheet">
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+
+    @include('layouts.inc.admin.sidebar')
+
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <!-- Navbar -->
+        @include('layouts.inc.admin.navbar')
+        <!-- End Navbar -->
+        <div class="container-fluid py-4">
+            @yield('content')
+            @include('layouts.inc.admin.footer')
+        </div>
+    </main>
+
+
+    {{-- Script --}}
+    <script src="{{ asset('admin/js/popper.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/bootstrap.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/perfect-scrollbar.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/smooth-scrollbar.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/chartjs.min.js') }}" defer></script>
+
+    @yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/inc/admin/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer pt-3">
+    <div class="container-fluid">
+        <div class="align-items-center justify-content-lg-between">
+            <div>
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>,
+                    made with <i class="fa fa-heart"></i> by
+                    <a href="https://www.facebook.com/castrichmauxanh/" class="font-weight-bold" target="_blank">Gnurt</a>
+                    for a better web.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
